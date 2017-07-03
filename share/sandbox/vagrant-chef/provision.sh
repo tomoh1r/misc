602ba0df74d987c890096c9765640534a0b7b6d4
Tomohiro NAKAMURA <tomohiro.nakamura@brainpad.co.jp>
2017-07-03 14:56:32 +0900

    Install chef with scls

--- share/sandbox/vagrant-chef/provision.sh
+++ share/sandbox/vagrant-chef/provision.sh
@@ -15,10 +15,8 @@ yum clean all
 
 yum makecache fast
 yum group install -y "Development Tools"
-yum install -y rh-ruby24-rubygem-bundler rh-ruby24-scldevel
+yum install -y rh-ruby24-rubygem-bundler rh-ruby24-ruby-devel
 
-[ ! -d /home/vagrant/.local ] && mkdir /home/vagrant/.local
-
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/rh/rh-ruby24/root/lib64 sudo -u vagrant /opt/rh/rh-ruby24/root/usr/bin/bundler install --gemfile=/vagrant/chef/Gemfile --path=/home/vagrant/.local/lib/ruby/gems --binstubs=/home/vagrant/.local/bin
+sudo -u vagrant scl enable rh-ruby24 'bundler install --gemfile=/vagrant/chef/Gemfile --path=/home/vagrant/.local/lib/ruby/gems --binstubs=/home/vagrant/.local/bin'
 
 yum clean all
