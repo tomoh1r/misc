7821de78d69c0c4df086c2e015b8bcb62b034b19
Tomohiro Nakamura <quickness.net@gmail.com>
2018-06-02 16:30:42 +0900

    Add share/ansible/provision.py del share/ansible/provision.sh

---
+++ share/ansible/provision.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals
+
+import argparse
+import os
+import shlex
+import subprocess
+
+
+class ProvisionCaller(object):
+    def __call__(self):
+        here = os.path.dirname(os.path.abspath(__file__))
+        self.rootdir = os.path.relpath(here)
+
+        proc = subprocess.Popen(shlex.split(self.build_cmds()))
+        proc.communicate()
+
+    def parse_args(self):
+        parser = argparse.ArgumentParser(
+            description='Wrapper for ansible-playbook.')
+        parser.add_argument(
+            '--check', '-C',
+            action='store_true',
+            help='add opts -C to ansible-playbook')
+        parser.add_argument(
+            '--diff', '-D',
+            action='store_true',
+            help='add opts -D to ansible-playbook')
+        parser.add_argument(
+            'tags', nargs='+', help='tags')
+        return parser.parse_args()
+
+    def build_cmds(self):
+        args = self.parse_args()
+        cmds = ['ansible-playbook -K -i {rootdir}/hosts']
+        if args.check:
+            cmds.append('--check')
+        if args.diff:
+            cmds.append('--diff')
+        cmds.append('-t "{tags}" {rootdir}/playbook.yml')
+        cmds = ' '.join(cmds)
+
+        return cmds.format(
+            rootdir=self.rootdir,
+            tags=','.join(args.tags))
+
+
+if __name__ == '__main__':
+    ProvisionCaller()()
