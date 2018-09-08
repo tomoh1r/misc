b10dd153c3adef7b4d708fb6119b8626e2ac7ad7
Tomohiro NAKAMURA <quickness.net@gmail.com>
2018-09-08 17:42:05 +0900

    Update flake8 execution

--- share/sandbox/python-flake8-zipappize/__main__.py
+++ share/sandbox/python-flake8-zipappize/__main__.py
@@ -4,5 +4,4 @@ sys.path = [x for x in sys.path if 'dist-packages' not in x]
 import pkg_resources
 pkg_resources.get_distribution('flake8')
 
-from flake8.main import cli
-cli.main()
+__import__('flake8.main', globals(), locals(), ['cli'], 0).cli.main()
