f7f9d90a5b7ca21260f41ff93ff94f99a48e56fc
Tomohiro NAKAMURA <quickness.net@gmail.com>
2018-09-08 19:37:54 +0900

    Update to shiv binary of flake8, pipenv

--- share/sandbox/python-flake8-zipappize/__main__.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path = [x for x in sys.path if 'dist-packages' not in x]
-
-import pkg_resources
-pkg_resources.get_distribution('flake8')
-
-__import__('flake8.main', globals(), locals(), ['cli'], 0).cli.main()
--- share/sandbox/python-pipenv-zipappize/__main__.py
+++
@@ -1,4 +0,0 @@
-import sys
-sys.path = [x for x in sys.path if 'dist-packages' not in x]
-
-__import__('pipenv.cli', globals(), locals(), ['cli'], 0).cli()
