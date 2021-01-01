2bd057e1338ccc3957a02fc7f963c3d337bc01c0
Tomohiro NAKAMURA <quickness.net@gmail.com>
2021-01-01 15:28:27 +0900

    fix win curl path.

--- cmd/curl-asjson.ps1
+++ cmd/curl-asjson.ps1
@@ -1,1 +1,1 @@
-curl.exe -H 'Content-type: application/json' -H 'Accept: application/json' $Args
+C:\Windows\System32\curl.exe -H 'Content-type: application/json' -H 'Accept: application/json' $Args
